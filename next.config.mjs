189de946903c49ba8f1de93905af78e2e6106a5e
Abbas Heydari <abbasheydari.110afg@gmail.com>
2026-09-14 23:50:07 +0430

    deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/coffeeShop" : "",
+  assetPrefix: isProd ? "/coffeeShop/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
